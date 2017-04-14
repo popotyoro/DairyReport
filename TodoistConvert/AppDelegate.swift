be7123f5d1dd566f993ee53ce58f9b8197f92969
鈴木 耕介 <kosuke-suzuki@navitime.co.jp>
2017-04-14 15:28:23 +0900

    Initial Commit

---
+++ TodoistConvert/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TodoistConvert
+//
+//  Created by 鈴木 耕介 on 2017/04/14.
+//  Copyright © 2017年 鈴木 耕介. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
